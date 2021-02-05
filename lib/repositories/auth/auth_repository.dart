eafd64a4a23cfd54f4e59732800fb2017d8318e8
Viktor Voitovich <viktor.voitovich@icloud.com>
2021-02-05 22:08:26 +0300

    feat: create Auth Repository

---
+++ lib/repositories/auth/auth_repository.dart
@@ -0,0 +1,100 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:meta/meta.dart';
+
+import '../../config/paths.dart';
+import '../../entitie/user_entity.dart';
+import '../../models/models.dart';
+import '../repositories.dart';
+
+class AuthRepository extends BaseAuthRepository {
+  final FirebaseFirestore _firestore;
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+
+  AuthRepository({
+    FirebaseFirestore firestore,
+    firebase_auth.FirebaseAuth firebaseAuth,
+  })  : _firestore = firestore ?? FirebaseFirestore.instance,
+        _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+
+  Future<User> _firebaseUserToUser(firebase_auth.User user) async {
+    DocumentSnapshot userDoc =
+        await _firestore.collection(Paths.users).doc(user.uid).get();
+
+    if (userDoc.exists) {
+      User user = User.fromEntity(UserEntity.fromSnapshot(userDoc));
+      return user;
+    }
+
+    return User(
+      id: user.uid,
+      email: '',
+    );
+  }
+
+  @override
+  Future<User> loginAnonymously() async {
+    final authResult = await _firebaseAuth.signInAnonymously();
+    return await _firebaseUserToUser(authResult.user);
+  }
+
+  @override
+  Future<User> signupWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) async {
+    final currentUser = _firebaseAuth.currentUser;
+    final authCredential = firebase_auth.EmailAuthProvider.credential(
+      email: email,
+      password: password,
+    );
+    final authResult = await currentUser.linkWithCredential(authCredential);
+    final user = await _firebaseUserToUser(authResult.user);
+
+    _firestore
+        .collection(Paths.users)
+        .doc(user.id)
+        .set(user.toEntity().toDocument());
+
+    return user;
+  }
+
+  @override
+  Future<User> loginWithEmailAndPassword({
+    @required String email,
+    @required String password,
+  }) async {
+    final authResult = await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return await _firebaseUserToUser(authResult.user);
+  }
+
+  @override
+  Future<User> logout() async {
+    await _firebaseAuth.signOut();
+    return await loginAnonymously();
+  }
+
+  @override
+  void dispose() {}
+
+  @override
+  Future<User> getCurrentUser() async {
+    final currentUser = _firebaseAuth.currentUser;
+
+    if (currentUser == null) {
+      return null;
+    }
+
+    return await _firebaseUserToUser(currentUser);
+  }
+
+  @override
+  Future<bool> isAnonymous() async {
+    final currentUser = _firebaseAuth.currentUser;
+    return currentUser.isAnonymous;
+  }
+}
